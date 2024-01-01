1b0ab0ed810966ba66b8468b9d266a8a956dfefb
ajlaroya <arthur.j.laroya@gmail.com>
2024-01-01 11:35:07 +1100

    new project, new me

---
+++ app/page.jsx
@@ -0,0 +1,32 @@
+export default function Home() {
+  return (
+    <main>
+      {/* Island */}
+      {/* Hero */}
+      <div className="bg-gradient flex flex-col items-center min-h-full pt-32 pb-44 md:pb-64">
+        <div className="hero">
+          <div className="relative">
+            {/* Title */}
+            <div className="title mix-blend-overlay text-[75px] text-black tracking-tight text-center transition--fade-up on-load delay-[2.5s]">
+              Arthur Laroya
+            </div>
+          </div>
+          <div className="h-[320px]">
+
+          </div>
+        </div>
+        {/* Headline */}
+        <div className="text-[24px] leading-[120%] max-w-[640px] md:max-w-[928px] mx-8 text-center relative">
+          <div className="mix-blend-overlay tracking-tight">
+            This is your friend who scores the hottest tables in town, your
+            passport to weekend getaways, and your guest pass to must-see
+            events. All brilliantly brought together in a card and app of
+            uncompromising beauty and utility. This is the beginning of a
+            journey to a world with no limits.
+          </div>
+        </div>
+      </div>
+      {/* Section 1 */}
+    </main>
+  );
+}
